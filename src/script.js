204a8c1c76e36370608cfd55c69c662aeb26e5ea
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 13:31:23 +0530

    Task 52

--- src/script.js
+++ src/script.js
@@ -237,4 +237,20 @@ function reusableFunction() {
   
   const answer = timesFive(5);
 
-  
+
+
+
+let myGlobal = 10;
+function fun1() {
+    oopsGlobal = 5;
+}
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
