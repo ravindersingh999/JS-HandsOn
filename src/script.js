ee1186556849a5cf6df08c996efc72f9f8e094ab
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 12:17:08 +0530

    Task 36

--- src/script.js
+++ src/script.js
@@ -162,3 +162,9 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
