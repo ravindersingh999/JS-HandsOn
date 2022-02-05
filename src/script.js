e7c1d6a2b59330e558a98db67708f64297d93600
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 12:48:49 +0530

    Task 41

--- src/script.js
+++ src/script.js
@@ -177,3 +177,7 @@ const wordBlanks = myNoun+" "+myAdjective+" "+myVerb+" "+myAdverb;
 const myArray = ["Ravinder", 22];
 
 const myArray = [["Bulls", 23], ["White Sox", 45]];
+
+
+const myArray = [50, 60, 70];
+var myData = myArray[0];
