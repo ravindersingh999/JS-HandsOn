5f9bc1d4430eaeb45184041e9f22cc40b3f374d9
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 13:45:33 +0530

    Task 55

--- src/script.js
+++ src/script.js
@@ -266,10 +266,22 @@ console.log(myStr,someAdjective);
 
 
 
-var outerWear = "T-Shirt";
-function myOutfit() {
-    var outerWear = "sweater";
-    return outerWear;
-}
+// var outerWear = "T-Shirt";
+// function myOutfit() {
+//     var outerWear = "sweater";
+//     return outerWear;
+// }
 
-myOutfit();
+// myOutfit();
+
+
+
+let sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+function addFive(){
+  sum = sum +5;
+}
+addThree();
+addFive();
