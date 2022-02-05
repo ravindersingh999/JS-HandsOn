e9b5aff92814270ad0fa0440edf5e6acf6b89d26
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 15:01:35 +0530

    Task 69

--- src/script.js
+++ src/script.js
@@ -444,3 +444,20 @@ function testStrictNotEqual(val) {
   }
   
   testLessOrEqual(10);
+
+
+
+
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val >= 25) {
+      return 'Yes';
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  // Change this value to test
+  testLogicalAnd(10);
