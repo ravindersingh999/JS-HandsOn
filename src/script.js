857b1c5a1afd3f5782c6d1faf89b77321b471407
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 14:59:43 +0530

    Task 68

--- src/script.js
+++ src/script.js
@@ -428,3 +428,19 @@ function testStrictNotEqual(val) {
   }
   
   testLessThan(10);
+
+
+
+  function testLessOrEqual(val) {
+    if (val<=12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val<=24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  testLessOrEqual(10);
