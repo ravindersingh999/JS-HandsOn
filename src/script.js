b991d679ff0e3da1441efdd85d1610b69e1c1352
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 14:51:54 +0530

    Task 64

--- src/script.js
+++ src/script.js
@@ -365,3 +365,15 @@ function testNotEqual(val) {
   }
   
   testNotEqual(10);
+
+
+
+  // Setup
+function testStrictNotEqual(val) {
+    if (val!== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
