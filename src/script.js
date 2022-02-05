52744d9215caae5693d831d276a87800d6ae7df8
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 15:04:48 +0530

    Task 70

--- src/script.js
+++ src/script.js
@@ -461,3 +461,19 @@ function testStrictNotEqual(val) {
   
   // Change this value to test
   testLogicalAnd(10);
+
+
+
+  function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val < 10 || val > 20) {
+      return "Outside";
+    }
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  // Change this value to test
+  testLogicalOr(15);
