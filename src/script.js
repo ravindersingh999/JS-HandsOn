4329d6bf256ac5c7821ad1ffe2494d4278aba842
Ravinder Singh <ravindersingh@cedcoss.com>
2022-02-05 15:11:35 +0530

    Task 73

--- src/script.js
+++ src/script.js
@@ -510,3 +510,16 @@ function testStrictNotEqual(val) {
     }
   
   }
+
+
+
+
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
